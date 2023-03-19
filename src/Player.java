75669efd80e384dcde744e095c9c09c105a68697
stlpr <stelios.parashos@gmail.com>
2023-03-19 13:29:49 +0200

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,27 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Player {
-    static String name;
-    static int position;
-    Player(String name,int position){
-        this.name = name;
-        this.position = position;
+    public static int giveNumOfPlayers() {
+        return GameCreation.givePlayers();
     }
-    public static void setName(String newName) {
+
+    private String name;
+    private int position;
+
+    public Player(String name) {
+        this.name = name;
+        this.position = 0; //Always starts from 0
+    }
+
+    public String toString() {
+        return "Name: " + name + ", Position: " + position;
+    }
+
+    public void setName(String newName) {
+
         name = newName;
     }
-    public static String getName() {
+
+    public String getName() {
+
         return name;
     }
 
-    public static int getPosition() {
+    public int getPosition() {
         return position;
     }
+
     public void setPosition(int newPosition) {
+
         position = newPosition;
     }
-    public static void  printName() {
+
+    public void printName() {
+
         System.out.println(name);
     }
-    public static void  printPosition() {
+
+    public void printPosition() {
         System.out.println(position);
+    }
+
+    static List<Player> listOfPlayers = new ArrayList<>();
+
+    public static void giveNamesToPlayers() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i < GameCreation.givePlayers() + 1; i++) {
+            System.out.print("Enter a Username for player" + i + ": ");
+            String getPlayerName = sc.nextLine();
+            Player newPlayer = new Player(getPlayerName);
+            newPlayer.setPosition(0);
+            listOfPlayers.add(newPlayer);
+        }
+        System.out.println("=======CURRENT PLAYERS=======");
+        int counter = 1;
+        for (Player c : listOfPlayers) {
+            System.out.println("Player" +counter+ ": " + c.getName());
+            counter++;
+
+        }
     }
 }
