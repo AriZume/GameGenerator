262269d49909bc8fc1b0d66478e7094588fb6b6a
AriZume <128248493+AriZume@users.noreply.github.com>
2023-03-20 22:09:20 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -30,5 +30,14 @@ public class Main {
             System.out.println(name);
             System.out.println(playerPos);
         }
+
+        int amountOfDice = input.nextInt();
+        while(amountOfDice >2 || amountOfDice <1) {
+            System.out.println("something went wrong or you enter less than 1 or more than 2 enter agein :");
+            amountOfDice = input.nextInt();
+        }
+        Dice amountDice = new Dice(amountOfDice);
+        int Roll = amountDice.getTotal();
+        System.out.println(Roll);
     }
 }
