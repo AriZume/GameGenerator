347ae87d84fd5e41e74f9822494f69d731711cce
AriZume <128248493+AriZume@users.noreply.github.com>
2023-03-20 23:06:39 +0200

    update

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,31 @@
 package MainPackage;
 
+import java.util.ArrayList;
+
 public class Board {
+    private ArrayList<Tile> totalTiles = new ArrayList<>();
+    private ArrayList<Player> totalPlayers;
+
+    public Board()
+    {
+        totalPlayers = new ArrayList<>();
+    }
+
+    public void addPlayer(Player newPlayer)
+    {
+        totalPlayers.add(newPlayer);
+    }
+
+    public void showPlayer()
+    {
+        int count =0;
+        for(Player currentPlayer : totalPlayers)
+        {
+            count++;
+            String name = currentPlayer.getName();
+            int pos = currentPlayer.getCurrentPosition();
+            System.out.println("Player" + count +": " +name );
+            System.out.println("Position: " +pos );
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        ArrayList<Player> currentPlayers = new ArrayList<>();
+        Board myBoard = new Board();
 
         Scanner input = new Scanner(System.in);
         int count =1;
@@ -15,25 +15,18 @@ public class Main {
             System.out.println("Player" + count + ": ");
             String name = input.nextLine();
             count++;
+            myBoard.addPlayer(new Player(name));
             if(name.isEmpty())
             {
                 break;
             }
-            currentPlayers.add(new Player(name));
         }
+        myBoard.showPlayer();
 
-        System.out.println("Current Players: ");
-        for(Player currentPlayer : currentPlayers)
-        {
-            String name = currentPlayer.getName();
-            int playerPos = currentPlayer.getCurrentPosition();
-            System.out.println(name);
-            System.out.println(playerPos);
-        }
-
+// Dice example
         int amountOfDice = input.nextInt();
         while(amountOfDice >2 || amountOfDice <1) {
-            System.out.println("something went wrong or you enter less than 1 or more than 2 enter agein :");
+            System.out.println("something went wrong or you enter less than 1 or more than 2 enter again :");
             amountOfDice = input.nextInt();
         }
         Dice amountDice = new Dice(amountOfDice);
