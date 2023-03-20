4d9c0b78236ac0e99be2dae98c26a744e24aa27d
AriZume <128248493+AriZume@users.noreply.github.com>
2023-03-20 20:51:18 +0200

    New Project Files

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+package MainPackage;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        ArrayList<Player> currentPlayers = new ArrayList<>();
+
+        Scanner input = new Scanner(System.in);
+        int count =1;
+        while(true)
+        {
+            System.out.println("Player" + count + ": ");
+            String name = input.nextLine();
+            count++;
+            if(name.isEmpty())
+            {
+                break;
+            }
+            currentPlayers.add(new Player(name));
+        }
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,19 @@
+package MainPackage;
+
+public class Player {
+    private String name;
+    private int currentPosition;
+    public Player(String n)
+    {
+        name = n;
+        currentPosition = 0;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+
+
+}
