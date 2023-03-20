148341b74b42d36e9c41633cbe7117e0937f3ed1
kapadokos <kapadokos@yahoo.com>
2023-03-20 21:57:57 +0200

    dice

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,22 @@
 package MainPackage;
-
+import java.util.Random;
 public class Dice {
+    int diceAmount;
+
+    public Dice(int amount){
+        diceAmount = amount;
+    }
+    public int  rollDice(){
+               Random roll = new Random();
+               int dice = roll.nextInt(6)+1;
+               return dice;
+    }
+    public int getTotal(){
+        if (diceAmount == 2 ){
+            return  rollDice()+rollDice();
+        }
+        else {
+            return rollDice();
+        }
+    }
 }
