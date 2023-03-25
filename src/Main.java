1d89ef834500c6cae6281448a163305224c63e45
AriZume <128248493+AriZume@users.noreply.github.com>
2023-03-25 23:28:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,35 +7,12 @@ public class Main
     {
         GameHandler myGame;
         Board myBoard = new Board();
+        MainMenuScreen myMenu = new MainMenuScreen();
+
         Scanner input = new Scanner(System.in);
 
-        System.out.println("\t\t  MENU\n========================\n1. Design Game and Play\n2. Load Game\n3. Help\n4. Exit");
         int EXIT = 4;
-        int userOption;
-        int playerAmount, diceAmount, tileAmount;
-
-        //TODO: DELETE PREVIOUS OUTPUT IN CONSOLE
-        do
-        {
-            while (!input.hasNextInt())
-            {
-                System.out.print("Please select one of the following options: ");
-                input.next();
-                System.out.println();
-            }
-            userOption = input.nextInt();
-
-            if (userOption == 2)
-            {
-                System.out.print("Loading is not available. Try a different option: ");
-                userOption = -1; // Probably wrong way to do it, but works
-            }
-            else if(userOption > 4 || userOption <= 0)
-            {
-                System.out.print("\nInvalid option.\nPlease select one of the following options: ");
-            }
-        } while (userOption > 4 || userOption <= 0);
-
+        int userOption = myMenu.getOptionMainMenu();
 
         while(userOption != EXIT)
         {
@@ -44,81 +21,31 @@ public class Main
             {
                 case 1:
                     System.out.println("\t  GAME DESIGN\n========================");
-                    System.out.print("Give player amount : ");
+                    myMenu.setPlayersAmount();
+                    myMenu.setTileAmount();
+                    myMenu.setDiceAmount();
 
-                    do
+                    myGame = new GameHandler(myMenu.getDiceAmount(), myBoard);
+
+                    System.out.println("---Enter names---");
+                    for (int i = 0; i < myMenu.getPlayersAmount(); i++)
                     {
-                        while (!input.hasNextInt())
-                        {
-                            System.out.print("Player amount should be 2 or more: ");
-                            input.next();
-                        }
-
-                        playerAmount = input.nextInt();
-                        if (playerAmount < 2)
-                        {
-                            System.out.print("Player amount should be 2 or more: ");
-                        }
-                    } while (playerAmount < 2);
-
-                    System.out.print("Give tile amount : ");
-
-                    do
-                    {
-                        while (!input.hasNextInt())
-                        {
-                            System.out.print("Tiles should be 1 or more: ");
-                            input.next();
-                        }
-
-                        tileAmount = input.nextInt();
-                        if (tileAmount < 1)
-                        {
-                            System.out.print("Tiles should be 1 or more: ");
-                        }
-                    } while (tileAmount < 1);
-
-                    System.out.print("Give dice amount : ");
-
-                    do
-                    {
-                        while (!input.hasNextInt())
-                        {
-                            System.out.print("Dice amount should be 1 or more: ");
-                            input.next();
-                        }
-
-                        diceAmount = input.nextInt();
-                        if (diceAmount < 1)
-                        {
-                            System.out.print("Dice amount should be 1 or more: ");
-                        }
-                    } while (diceAmount < 1);
-
-                    myGame = new GameHandler(diceAmount, myBoard);
-                    System.out.println("Enter names");
-                    input.nextLine(); //Probably not correct but works
-
-                    for (int i = 0; i < playerAmount; i++)
-                    {
-                        System.out.print("Player" + (i + 1) + " : ");
+                        System.out.print("Player " + (i + 1) + " : ");
                         String name = input.nextLine();
 
                         myGame.addPlayer(new Player(name));
                     }
 
-                    for (int i = 1; i <= tileAmount; i++)
+                    for (int i = 1; i <= myMenu.getTileAmount(); i++)
                     {
                         myBoard.addTileNumber(new Tile(i));
                     }
 
                     myGame.startGame();
                     break;
-                /*
                 case 2:
                     System.out.println("Load is currently unavailable.");
                     break;
-                    */
                 case 3:
                     System.out.println("Can't give you help :(\nPlease donate $19.99 to unlock GameGenerator Premium.");
                     break;
@@ -127,29 +54,7 @@ public class Main
                     break;
             }
 
-            System.out.println("\t\t  MENU\n========================\n1. Design Game and Play\n2. Load Game\n3. Help\n4. Exit");
-
-
-
-            do
-            {
-                while (!input.hasNextInt())
-                {
-                    System.out.print("Please select one of the other options: ");
-                    input.next();
-                }
-                userOption = input.nextInt();
-
-                if (userOption == 2)
-                {
-                    System.out.print("Loading is not available. Try a different option: ");
-                    userOption = -1; // Probably wrong way to do it, but works
-                }
-                else if(userOption > 4 || userOption <= 0)
-                {
-                    System.out.print("\nInvalid option.\nPlease select one of the other options: ");
-                }
-            } while (userOption > 4 || userOption <= 0);
+            userOption = myMenu.getOptionMainMenu();
         }
     }
 }
