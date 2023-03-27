e90a9ae359bd762cee7ca05cc49249ff7bbbdbb4
Tziouve <steliostz2002@gmail.com>
2023-03-27 18:25:53 +0300

    Tiles arxh

--- src/Tile.java
+++ src/Tile.java
@@ -1,12 +1,16 @@
 package MainPackage;
 
+import java.util.Random;
+
 public class Tile {
 
     private final int tileNumber;
+    private int tilePower;
 
     public Tile(int number)
     {
         tileNumber = number;
+        tilePower = 0;
     }
 
     public int getTileNumber()
@@ -14,4 +18,23 @@ public class Tile {
         return tileNumber;
     }
 
+    public int getTilePower()
+    {
+        return tilePower;
+    }
+
+    public void setTilePower(int positiveNumber, int negativeNumber)
+    {
+        Random tzegkas = new Random();
+        int ranNum = tzegkas.nextInt(2) + 1;
+
+        if (ranNum == 1)
+        {
+            tilePower = tzegkas.nextInt(positiveNumber) + 1;
+        } else
+        {
+            tilePower = tzegkas.nextInt(negativeNumber) - 1;
+        }
+
+    }
 }
