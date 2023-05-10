5e0190fd3b87da96ab66cdc13bc77e65b7f311fa
AriZume <128248493+AriZume@users.noreply.github.com>
2023-05-10 18:19:17 +0300

    update

--- New Game Generator/New Game Generator (Petalidis)/src/LoseTurnTile.java
+++
@@ -1,9 +0,0 @@
-public class LoseTurnTile extends Tile
-{
-    @Override
-    public Response updatePlayerStatus(Player player)
-    {
-        player.setCurrentPosition(player.getCurrentPosition() - player.getPersonalRoll());
-        return new Response("\033[31m" + "You lose your turn. " + "\033[0m");
-    }
-}
--- New Game Generator/New Game Generator (Petalidis)/src/Tile.java
+++
@@ -1,8 +0,0 @@
-public class Tile
-{
-    public Response updatePlayerStatus(Player p)
-    {
-        return new Response("This tile has no effect.");
-
-    }
-}
